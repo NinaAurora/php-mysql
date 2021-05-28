8b50044c64c23055e1402241ea6c06a5bf0ad6b6
NinaAurora <ninika@gmail.com>
2021-05-28 14:49:26 +0200

    Changed some colors

--- views/skills/index.view.php
+++ views/skills/index.view.php
@@ -5,14 +5,24 @@
     <h1> Skills </h1>
     <hr>
 
-    <?php foreach($vars['skills'] as $skill) : ?>
-        <div> <?= $skill["skill_name"] ?> </div> 
-        <div> <?= $skill["rating"] ?> </div> 
-    <?php endforeach ?>
+    <div class="row justify-content-content">
+        <div class="col-6">
+
+            <?php foreach($vars['skills'] as $skill) : ?>
+            <div> <?= $skill["skill_name"] ?> </div>
+            <?php endforeach ?>
+
+        </div>
+        <div class="col-6 text-center">
+
+            <?php foreach($vars['skills'] as $skill) : ?>
+            <div class="stars"> <?= $skill["rating"] ?> </div>
+            <?php endforeach ?>
+
+        </div>
+    </div>
 
 </div>
-
-
 
 
 <?php require 'views/partials/footer.view.php' ?>
